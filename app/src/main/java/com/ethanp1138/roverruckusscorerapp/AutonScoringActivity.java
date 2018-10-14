506d722854e33627d4b4575582e94e5eee1539c9
Ethan Pang <ethanp1138@gmail.com>
2018-10-14 12:26:05 -0700

    basic formatting for scoring activity

--- app/src/main/java/com/ethanp1138/roverruckusscorerapp/AutonScoringActivity.java
+++ app/src/main/java/com/ethanp1138/roverruckusscorerapp/AutonScoringActivity.java
@@ -2,11 +2,13 @@ package com.ethanp1138.roverruckusscorerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AutonScoringActivity extends AppCompatActivity {
 
-    private static final String[] SCORINGACHIEVEMENTS = {"Landed", "Depot Claimed", "Parked in Crater", "Gold Mineral Sampled"};
+    private static String[] SCORINGACHIEVEMENTS = {"Landed", "Depot Claimed", "Parked in Crater", "Gold Mineral Sampled"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class AutonScoringActivity extends AppCompatActivity {
         //get layout of the scoring activity
         LinearLayout layout = findViewById(R.id.activity_auton_scoring);
 
+
         //create a label and two checkboxes for each scoring achievement
         for(String achievement : SCORINGACHIEVEMENTS){
             addScoringAchievement(achievement, layout);
@@ -24,5 +27,14 @@ public class AutonScoringActivity extends AppCompatActivity {
 
     private void addScoringAchievement(String achievement, LinearLayout layout){
 
+        //set up scoring to screen wih the scoring_achievement.xml
+        View scoring = getLayoutInflater().inflate(R.layout.scoring_achievement,null);
+
+        //get widget with id to change label text
+        TextView tv = scoring.findViewById(R.id.scoring_text);
+        tv.setText(achievement);
+
+        //add widget to layout
+        layout.addView(scoring);
     }
 }
