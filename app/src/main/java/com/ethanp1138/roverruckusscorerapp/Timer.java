b7406ccf2ed14a2e61a2e7127a9ef843bd8b0d8d
Ethan Pang <ethanp1138@gmail.com>
2018-12-22 14:18:43 -0800

    change app launcher icon

--- app/src/main/java/com/ethanp1138/roverruckusscorerapp/Timer.java
+++ app/src/main/java/com/ethanp1138/roverruckusscorerapp/Timer.java
@@ -5,16 +5,16 @@ import android.widget.Toast;
 
 public class Timer {
 
-    private final int maxTime = 30000;
-    private int time = 30000;
-    boolean startFromBeginning = true;
+    private final int maxTime = 30000; // auton time limit is 30 seconds
+    private int time = 30000; // holds current time
+    boolean startFromBeginning = true; // determines whether to begin timer from 30 sec or not
     AutonScoringActivity activity = null;
-    CountDownTimer timer = null;
+    CountDownTimer timer = null; // android os timer
     public Timer(AutonScoringActivity activity){
         this.activity = activity;
-    }
+    } // constructor
 
-
+    // starts timer from beginning or from where it left off depending on boolean: startFromBeginning
     public void startTimer(){
         if(startFromBeginning){
             initTimer(maxTime);
@@ -24,16 +24,18 @@ public class Timer {
             resumeTimer(time);
         }
     }
+    // starts a timer from the current time
     private void resumeTimer(int currentTime){
         initTimer(currentTime);
         timer.start();
     }
-
+    // stops timer and start a new one from beginning
     public void restartTimer(){
         stopTimer();
         initTimer(maxTime);
         startFromBeginning = true;
     }
+    // makes a new timer with given start time
     private void initTimer(int startTime){
         timer = new CountDownTimer(startTime, 1000) {
             @Override
@@ -49,7 +51,10 @@ public class Timer {
             }
         };
     }
+    // stops timer
     public void stopTimer(){
-        timer.cancel();
+        if(timer != null) {
+            timer.cancel();
+        }
     }
 }
